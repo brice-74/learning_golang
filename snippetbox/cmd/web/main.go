237830691111bd3310671fe2f4c50edecbc9a0c4
LaFripouille <71272519+brice-74@users.noreply.github.com>
2021-07-28 17:26:43 +0200

    snippetbox: dependency injection

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,9 +7,9 @@ import (
 	"os"
 )
 
-type Config struct {
-	Addr      string
-	StaticDir string
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
 }
 
 type justFilesFilesystem struct {
@@ -33,21 +33,33 @@ func (fs justFilesFilesystem) Open(name string) (http.File, error) {
 }
 
 func main() {
-	cfg := new(Config)
 
-	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static assets")
-	flag.Parse()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+  flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+  errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(justFilesFilesystem{http.Dir("./ui/static/")})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on %s", cfg.Addr)
-	err := http.ListenAndServe(cfg.Addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
