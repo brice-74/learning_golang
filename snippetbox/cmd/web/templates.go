5af7de7ef00027fa2dd88bda772a63cbabe80158
LaFripouille <71272519+brice-74@users.noreply.github.com>
2021-08-16 13:05:43 +0200

    snippetbox: custom template func

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
   "html/template"
   "path/filepath"
+  "time"
 
   "brice.local/snippetbox/pkg/models"
 )
@@ -11,6 +12,19 @@ type templateData struct {
   CurrentYear int
   Snippet *models.Snippet
   Snippets []*models.Snippet
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+  return t.Format(time.RFC822)
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+  "humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -23,10 +37,9 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
   }
 
   for _, page := range pages {
-
     name := filepath.Base(page)
 
-    ts, err := template.ParseFiles(page)
+    ts, err := template.New(name).Funcs(functions).ParseFiles(page)
     if err != nil {
       return nil, err
     }
